dd8ffadf4115366ffcb4d8de4230f7cc37ae8586
hafaeru <rafaelreis281@gmail.com>
2025-07-16 12:28:16 -0300

    Corrige o hostname para imagens do Supabase

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'yizprirfmglagpigsrny.supabase.co', // <-- CORRIGIDO AQUI
+        port: '',
+        pathname: '/storage/v1/object/public/fotos-pecas/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
